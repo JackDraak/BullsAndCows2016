34dc492bde6e52eedfe8a034b20f6766a1c949d1
JackDraak <jackdraak@gmail.com>
2016-01-08 19:14:54 -0500

    fix tabs

--- Section02/BullsAndCows2016/FBullCowGame.cpp
+++ Section02/BullsAndCows2016/FBullCowGame.cpp
@@ -1,9 +1,9 @@
 #include "FBullCowGame.h"
 
 void FBullCowGame::ExpendGuess()					{ MyCurrentTry++; return; }
-void FBullCowGame::Reset()							{ MyCurrentTry = 1;	return; }
+void FBullCowGame::Reset()						{ MyCurrentTry = 1;	return; }
 int FBullCowGame::GetMaxTries()						{ return MyMaxTries; }
 int FBullCowGame::GetCurrentTry()					{ return MyCurrentTry; }
-bool FBullCowGame::CheckGuessValidity(std::string)	{ return false; }
+bool FBullCowGame::CheckGuessValidity(std::string)			{ return false; }
 bool FBullCowGame::IsGameWon()						{ return false; }
-std::string FBullCowGame::PickTargetWord()			{ return std::string(); }
+std::string FBullCowGame::PickTargetWord()				{ return std::string(); }
