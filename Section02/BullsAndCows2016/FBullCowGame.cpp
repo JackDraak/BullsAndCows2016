ed0dda0de495cf1178f2670eed6a10069ff7d8da
JackDraak <jackdraak@gmail.com>
2016-01-09 00:29:11 -0500

    tidy formatting

--- Section02/BullsAndCows2016/FBullCowGame.cpp
+++ Section02/BullsAndCows2016/FBullCowGame.cpp
@@ -1,9 +1,9 @@
 #include "FBullCowGame.h"
 
-void FBullCowGame::ExpendGuess()						{ MyCurrentTry++; return; }
-void FBullCowGame::Reset()								{ MyCurrentTry = 1; return; }
-int FBullCowGame::GetMaxTries() const					{ return MyMaxTries; }
+void FBullCowGame::ExpendGuess()				{ MyCurrentTry++; return; }
+void FBullCowGame::Reset()					{ MyCurrentTry = 1; return; }
+int FBullCowGame::GetMaxTries() const				{ return MyMaxTries; }
 int FBullCowGame::GetCurrentTry() const				{ return MyCurrentTry; }
-bool FBullCowGame::IsGameWon() const					{ return false; }
+bool FBullCowGame::IsGameWon() const				{ return false; }
 bool FBullCowGame::CheckGuessValidity(std::string)		{ return false; }
-std::string FBullCowGame::PickTargetWord()				{ return std::string(); }
+std::string FBullCowGame::PickTargetWord()			{ return std::string(); }
